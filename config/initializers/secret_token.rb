5da49021b23039c6019d542757b1a778ef43069b
Nate Kane <nathanaelkane@gmail.com>
2011-04-19 10:05:44 +1000

    Cleaned out initializers directory.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Postmarkdown::Application.config.secret_token = '4722e123d157e79acbded087c05b95063c643ae1a1d334d53967f363f928105aa56d4c51b55443dbf000656edcac5569b199693affdbbe51eef83a5d0c03f0e0'
