481fa1454aee686ad6e8b2aeefa0cd08e7d60b6f
Nate Kane <nathanaelkane@gmail.com>
2011-04-21 12:37:23 +1000

    Added optional day param to the `posts#index` route.

--- lib/postmarkdown/routes.rb
+++ lib/postmarkdown/routes.rb
@@ -1,7 +1,7 @@
 class ActionDispatch::Routing::Mapper
   def postmarkdown(options = {})
     options.reverse_merge!({ :as => :posts })
-    get "/#{options[:as]}(/:year(/:month))" => 'posts#index', :as => :posts, :constraints => { :year => /\d{4}/, :month => /\d{2}/}
+    get "/#{options[:as]}(/:year(/:month(/:day)))" => 'posts#index', :as => :posts, :constraints => { :year => /\d{4}/, :month => /\d{2}/, :day => /\d{2}/}
     get "/#{options[:as]}/*id" => 'posts#show', :as => :post, :constraints => { :id => %r[\d{4}/\d{2}/\d{2}/[^/]+] }
     get "/#{options[:as]}/feed" => 'posts#feed', :as => :posts_feed, :format => :xml
   end
