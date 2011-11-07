eeff95f33288ad1e96fafa107f4660172deda399
Jason Weathered <jason@jasoncodes.com>
2011-11-07 22:11:10 +1000

    Share posts, routes and views between Combustion and Rails 3.0 test app.

---
+++ spec/internal/config/environments/test.rb
@@ -0,0 +1,9 @@
+Rails.application.class.configure do
+  config.cache_classes = true
+  config.whiny_nils = true
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching = false
+  config.action_dispatch.show_exceptions = false
+  config.action_mailer.delivery_method = :test
+  config.active_support.deprecation = :stderr
+end
