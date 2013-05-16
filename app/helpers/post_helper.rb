436dc34879123894f423d462d68ee7a5961b4da0
Rodrigo Martins <rodrigo@rrmartins.com>
2013-05-16 15:03:50 -0300

    change validation html

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -7,11 +7,12 @@ module PostHelper
     if post.summary.present?
       content_tag :p, post.summary
     else
-      html = Sanitizer.new.sanitize(post_content_html(post))
+      reg = /<!--more-->/
+      html = post_content_html(post)
 
+      html = html[0..(html =~ reg)-1]
       doc = Nokogiri::HTML.fragment(html)
-      para = doc.search('p').detect { |p| p.text }
-      para.try(:to_html).try(:html_safe)
+      doc.try(:to_html).try(:html_safe)
     end
   end
 
