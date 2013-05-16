ad099117d82feae2516c69d3e3d986479a3d9e6f
Rodrigo Martins <rodrigo@rrmartins.com>
2013-05-16 15:31:02 -0300

    change markdown file and validate of paragraph

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -8,10 +8,11 @@ module PostHelper
       content_tag :p, post.summary
     else
       reg = /<!--more-->/
-      html = post_content_html(post)
+      html_text = post_content_html(post)
 
-      html = html[0..(html =~ reg)-1]
+      html = !((html_text =~ reg).nil?) ? html_text[0..(html_text =~ reg)-1] : html_text
       doc = Nokogiri::HTML.fragment(html)
+      doc = doc.search('p').detect { |p| p.text.present? } if (html_text =~ reg).nil?
       doc.try(:to_html).try(:html_safe)
     end
   end
