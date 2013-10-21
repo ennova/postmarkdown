98612d393767b1e81498b1f9a3d8a9a6c0f18041
Jason Weathered <jason@jasoncodes.com>
2013-10-22 06:31:00 +1000

    WIP: Rails 4 support.

--- spec/internal/config/environments/test.rb
+++ spec/internal/config/environments/test.rb
@@ -1,6 +1,8 @@
 Rails.application.class.configure do
   config.cache_classes = true
-  config.whiny_nils = true
+  config.whiny_nils = true if Rails::VERSION::MAJOR < 4
+  config.secret_key_base = 'abc123' if Rails::VERSION::MAJOR >= 4
+  config.eager_load = false
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.action_dispatch.show_exceptions = false
